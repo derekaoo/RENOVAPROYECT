6661343e0fe26238475fc535eaf5933dd7cdfa38
DiegoRios546 <118926566+DiegoRios546@users.noreply.github.com>
2023-06-12 11:22:27 -0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,62 @@
+
+    <nav class="navbar navbar-light shadow" style="background-color: #fd5100; position: sticky; top: 0; z-index: 1;">
+        <div class="container d-flex">
+        <button class="navbar-toggler border-0 " type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand align-self-center" href="index.php">
+               <img src="assets/img/logo.png" alt="" style="width: 2.5rem;">
+            </a>
+
+            <a href="" class="text-decoration-none text-dark"><p >
+            <i class="fa fa-fw fa-solid fa-map text-white"></i> 
+            Oficina:
+            Paseo de las Parras #229 <br>
+            Valle Verde 34284 Durango, Dgo.</p>  
+            </a>
+        
+                    <div class="input-group" style="width: 40%; margin:auto;">
+                            <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ...">
+                            <div class="input-group-text">
+                                <i class="fa fa-fw fa-search"></i>
+                            </div>
+                        </div>
+
+        
+                    <a class="" href="#">
+                        <i class="fa fa-lg fa-cart-arrow-down text-dark"></i>
+                        <span class="top-0 translate-middle badge rounded-pill bg-light text-dark">7</span>
+                    </a>
+                    <a class="" href="#">
+                        <i class="fa fa-lg fa-user text-dark"></i>
+                        <span class="top-0 translate-middle badge rounded-pill bg-light text-dark">+99</span>
+                    </a>
+        </div>
+
+            <div class="align-self-center collapse navbar-collapse flex-fill justify-content-lg-between" id="templatemo_main_nav">
+                <div class="flex-fill">
+                    <ul class="nav navbar-nav d-flex">
+                        <li class="nav-item m-1" >
+                            <a class="nav-link" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="shop.php">Tienda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        
+
+
+                </div>
+    </nav>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js">
+        
+    </script>
+        <!-- Close Header -->
