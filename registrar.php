6ee39c182dcd7b5927728cb68e50b7faa9eb3fc7
derekaoo <109321952+derekaoo@users.noreply.github.com>
2023-06-23 10:44:18 -0600

    Add files via upload
    actulización de login

--- registrar.php
+++ registrar.php
@@ -51,13 +51,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
   <meta charset="UTF-8" />
-  <title>Iniciar sesión</title>
+  <title>Registrarse</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" type="text/css" href="assets/css/styles1.css" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,700,800,900"
-    rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,700,800" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1" />
 </head>
@@ -80,8 +79,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div> -->
 
   <div class="login-container-up">
-    <h2>¡BIENVENIDO!</h2>
-    <h4>Ingresa a tu cuenta de RENOVA DEPOT</h4>
+    <h2>¡REGÍSTRATE!</h2>
+    <h4>Creaa tu cuenta para Renova Depot</h4>
   </div>
 
   <div class="login-container">
@@ -90,25 +89,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <div class="input-wrapper">
         <input type="email" class="input" id="email" placeholder="Correo electrónico" name="nombre" required />
         <i class="fa-solid fa-user fa-sm icon-usr" style="color: #fd5100;"></i>
-        <input type="password" class="input" id="password" placeholder="Contraseña" name="password" required />
-        <i class="fa-solid fa-key fa-sm icon-pw" style="color: #fd5100;"></i>
+          <input type="password" class="input" id="password" placeholder="Contraseña" name="password" required />
+          <i class="fa-solid fa-key fa-sm icon-pw" style="color: #fd5100;"></i>
       </div>
-      <button type="submit" class="login-btn" value="iniciar sesion" name="btningresar">Iniciar sesión</button>
-    <div class="forgot-password">
-      <a href="#">Olvidé mi contraseña</a>
-    </div>
+      <button type="submit" class="login-btn" value="iniciar sesion" name="btningresar">Crear cuenta</button>
     </form>
-  
     <br>
     <hr>
     <br>
     <div class="social-login">
-      <button class="icono-boton"><i class="fa-brands fa-facebook fa-2xl"></i></button>
-      <button class="icono-boton"><i class="fa-brands fa-google fa-2xl"></i></button>
+      <button class="icono-boton"><i class="fa-brands fa-facebook fa-2xl" style="color: #1d2f4e"></i></button>
+      <button class="icono-boton"><i class="fa-brands fa-google fa-2xl" style="color: #1d2f4e"></i></button>
     </div>
 
     <div class="register">
-      <p>¿Aún no tienes cuenta? <a href=" registrar.php">Regístrate</a></p>
+      <p>¿Ya tienes cuenta? <a href="login.php">Inicia sesión</a></p>
     </div>
   </div>
 
